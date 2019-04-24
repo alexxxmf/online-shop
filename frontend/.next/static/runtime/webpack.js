b578502e04fd19a0f993bfb1f3071f1209e12319
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-04-24 13:23:29 +0200

    Adding graphql queries to client

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7a607c195f32e4fc2ad5";
+/******/ 	var hotCurrentHash = "21d5714dceb06af333b4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,38 @@
 import App, {Container} from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    /*
+    So basically the point here is leveraging on SSR capabilities of Next.js. With getInitialPrfops
+    we want to crawl all the pages, fetch the data we need and then pass it to the initial render
+    so we can have it both in client and server (next does that apparently)
+    Also this is the standard lifecycle method in next for this to do the heavylifting,
+    that's why we avoid componentDidMount I suppose
+    */
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        return { pageProps }
+    }
+
     render() {
-        const { Component } = this.props;
+        const { Component, apollo, pageProps } = this.props;
 
         return (
             <Container>
-                <Page>
-                    <Component/>
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp;
+export default withData(MyApp);

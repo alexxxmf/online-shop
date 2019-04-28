9906e46dc81e827ce54e75b5d051e859a16071c4
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-04-28 23:41:52 +0100

    Added feature to update item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,6 +4,21 @@ const Mutation = {
             data: { ...args }
         }, info); 
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        const updates = { ...args };
+        // removes the id from updates because we want to avoid
+        // updating it
+        delete updates.id;
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id
+                }
+            },
+            info
+        );
     }
 }
 
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "415b28585e2b5f52d396";
+/******/ 	var hotCurrentHash = "2a8d264bcafd05d75d8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

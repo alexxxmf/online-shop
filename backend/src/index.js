ecf030043641e5118dfba23876636cfdad0c5ecc
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-05-04 14:33:50 +0100

    Added feature to retrieve user data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,20 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
 const db = require('./db');
-
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
-// TODO Use express middlware to populate current user
+server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    req.userId = userId
+  }
+  next()
+})
 
 server.start(
   {

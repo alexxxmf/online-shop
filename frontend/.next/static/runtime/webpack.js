13337ece2964ca0b62e9cd2ad14da7111a1eabb2
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-05-05 11:53:53 +0100

    Added form to reset password

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3450b165fbc92141b326";
+/******/ 	var hotCurrentHash = "1f045d3701bef85bda69";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

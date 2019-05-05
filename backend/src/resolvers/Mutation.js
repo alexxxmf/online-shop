2822ce9ed78ced696e417737ce496a4a151d0b43
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-05-05 12:12:00 +0100

    Added feature to send mail with link to token

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeNiceEmail } = require('../mail')
 
 const Mutation = {
     async createItem(parent, args, ctx, info) {
@@ -106,8 +107,20 @@ const Mutation = {
           data: { resetToken, resetTokenExpiry },
         });
         console.log(res);
-        return { message: 'Thanks!' };
         // 3. Email them that reset token
+        const mailRes = await transport.sendMail({
+            from: 'alexmorofernandez@gmail.com',
+            to: user.email,
+            subject: 'Your password reset token',
+            html: makeNiceEmail(`
+                Your password reset token is here!
+                \n\n
+                <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to reset</a>
+            `)
+        })
+
+        return { message: 'Thanks!' };
+        
       },
       async resetPassword(parent, args, ctx, info) {
         // 1. check if the passwords match

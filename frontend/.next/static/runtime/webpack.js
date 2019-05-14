f51d0993dc27222acd45fd7d55413cd518c57de1
Alejandro Moro Fernandez <amoro@alumni.unav.es>
2019-05-14 22:05:00 +0100

    Added addTocart mutation

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1027622296111facbb3f";
+/******/ 	var hotCurrentHash = "4f7c5291c6f29e2aee69";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
